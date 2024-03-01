2085fb7a4433e96efa5e1d4c48af957c803ffa3a
Bachar Daowd <bachar.daowd@gmail.com>
2024-03-01 22:12:29 +0100

    edit main messages

--- backend/src/main/java/com/bachar/Main.java
+++ backend/src/main/java/com/bachar/Main.java
@@ -15,7 +15,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        System.out.println("Running the main Method");
+        System.out.println("Application is running");
     }
 
 
