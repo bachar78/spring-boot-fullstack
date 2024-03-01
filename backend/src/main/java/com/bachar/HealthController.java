a442ebaab2f97e622b0ec294a04ccaa16e35b337
bachardaowd <bachar.daowd@gmail.com>
2024-03-01 17:07:00 +0100

    Fix slack messages and add counter to health check

--- backend/src/main/java/com/bachar/HealthController.java
+++ backend/src/main/java/com/bachar/HealthController.java
@@ -4,21 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PingPongController {
-
-    record PingPong(String result) {
-    }
+public class HealthController {
+    private static int COUNTER = 0;
 
     record HealthStatus(String status) {
     }
 
-    @GetMapping("/ping")
-    public PingPong getPingPong() {
-        return new PingPong("Pong");
-    }
-
     @GetMapping("/health")
     public HealthStatus getStatus() {
-        return new HealthStatus("Up");
+        return new HealthStatus("Up: %s".formatted(++COUNTER) );
     }
 }
