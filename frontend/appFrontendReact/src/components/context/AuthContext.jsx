67a194c435ca9ae777ed6e5b5374e5a1a8c79184
Bachar Daowd <bachar.daowd@gmail.com>
2024-03-13 23:43:05 +0100

    add login form and create login service

---
+++ frontend/appFrontendReact/src/components/context/AuthContext.jsx
@@ -0,0 +1,68 @@
+import {useEffect, useState, useContext, createContext} from "react";
+import {login as performLogin} from "../../services/client.js";
+
+import {jwtDecode} from "jwt-decode"
+
+
+const AuthContext = createContext({})
+const AuthProvider = ({children}) => {
+    const [customer, setCustomer] = useState({})
+
+
+    useEffect(() => {
+        let token = localStorage.getItem("access_token")
+        if(token) {
+            token = jwtDecode(token);
+            setCustomer({
+                email: token.sub,
+                roles: token.scopes
+            })
+        }
+    }, []);
+    const login = async (emailAndPassword) => {
+        return new Promise((resolve, reject) => {
+            performLogin(emailAndPassword).then(res => {
+                const jwt = res.headers["authorization"];
+                localStorage.setItem("access_token", jwt);
+                const decodedJwt = jwtDecode(jwt);
+                setCustomer({
+                   ...res.data.customerDTO
+                })
+                resolve(res)
+            }).catch(err => {
+                reject(err);
+            })
+        })
+    }
+
+    const logout = () => {
+        localStorage.removeItem("access_token")
+        setCustomer(null)
+    }
+
+    const isCustomerAuthenticated = () => {
+        const token = localStorage.getItem("access_token")
+        if (!token) {
+            return false;
+        }
+        const {exp: expirationDate} = jwtDecode(token);
+        if (Date.now() > expirationDate * 1000) {
+            logout()
+            return false;
+        }
+        return true;
+    }
+
+
+    return (
+        <AuthContext.Provider value={{
+            customer, login, logout, isCustomerAuthenticated
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+
+export const useAuth = () => useContext(AuthContext)
+export default AuthProvider;
